b3a0920b510ac44d989db703688129ff256ca376
VaclavJirka <vaclav.jirka@icloud.com>
2025-01-10 22:38:35 +0100

    Automatically install dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@ sudo apt install -y python3-pip
 sudo apt-get install -y nano
 sudo apt-get install -y libcairo2-dev
 sudo apt install -y libgirepository1.0-dev
-sudo apt-get install libgstreamer1.0-dev
+sudo apt-get install -y libgstreamer1.0-dev
 sudo apt install -y wormhole
 sudo apt install -y python3-gst-1.0
 wget https://github.com/NVIDIA-AI-IOT/deepstream_python_apps/releases/download/v1.1.8/pyds-1.1.8-py3-none-linux_x86_64.whl
